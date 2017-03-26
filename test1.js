bb0a3136b4639ee4c2a1893c5e4d981c353a390a
Alex Kursell <alexkursell@gmail.com>
2017-03-25 20:43:21 -0400

    Started recursive searching function.

--- test1.js
+++ test1.js
@@ -3,59 +3,117 @@ jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 
 
-function NodeAttributes(nodeType, classes, id, index){
-	this.nodeType = nodeType;
-	this.classes = classes;
-  this.id = id;
-  this.index = index;
+function NodeAttributes(nodeType, classes, id, index) {
+    this.nodeType = nodeType;
+    this.classes = classes;
+    this.id = id;
+    this.index = index;
+    this.searchRank = null;
 }
-   
-function getSelectorPath(e){
-	if($(e).prop('tagName')){
-    //console.log(getFindSelector(e));
-    
-    path = getSelectorPath($(e).parent().closest('div'));
-    path.push(getFindSelector(e));
-    return path;
-  }
-  return [];
-};
 
-function getFindSelector(e){
-	var nodeType = $(e).prop('nodeName');
-  
-  var id = $(e).attr("id");
-  
-  if (!id){
-  	id = null;
-  }
+function AttributeComparator(o, n){
+	var os = 0;
+	var ns = 0;
 
-  var classNames = $(e).attr("class");
-  if (classNames){
- 		classNames = $.trim(classNames).replace(/\s\s+/g, ' ').replace(/\s/gi, ".").split(".");
-    } else {
-    	classNames = null;
+	//Compare classes
+	for(var i = 0; i < o.classes.length; i++){
+		os++;
+		if($.inArray(o.classes[i], n.classes) != -1){
+			ns++;
+		}
+	}
+
+	//Compare ids
+	os++;
+	if (n.id == o.id){
+		ns++;
+	}
+
+	//Compare indices
+	os++;
+	if(n.index == o.index){
+		ns++;
+	}
+
+	//Return what percentage of original attributes was found in the new element
+	return ns / os;
+}
+
+
+function recurseWalk(prev, attrs, idx){
+	var children = $(prev).children(attrs[idx].nodeType);
+
+	if (children.length < 1){
+		return null;
+	}
+
+	for(i = 0; i < children.length; i++){
+		children[i] = getFindSelector(children[i]);
+		children[i].searchRank = AttributeComparator(attrs[idx], children[i]);
+	}
+
+	children.sort(function(a, b){
+		return a.searchRank - b.searchRank;
+	});
+
+	if (children[children.length - 1].searchRank == 0){
+		return null;
+	}
+
+	
+
+
+
+
+}
+
+
+function getSelectorPath(e) {
+    if ($(e).prop('tagName')) {
+        //console.log(getFindSelector(e));
+
+        var path = getSelectorPath($(e).parent());
+        path.push(getFindSelector(e));
+        return path;
     }
-  
-  
-  idx = null;
-	similar = $(e).parent().closest('div').children(nodeType);
-  for(i = 0; i < similar.length; i++){
-  	if($(similar[i]).is($(e))){
-    	idx = i;
-      break;
-    };
-  };
-  
-  return new NodeAttributes(nodeType, classNames, id, idx);
+    return [];
+}
 
-};
+function getFindSelector(e) {
+    var nodeType = $(e).prop('nodeName');
+
+    var id = $(e).attr("id");
+
+    if (!id) {
+        id = null;
+    }
+
+    var classNames = $(e).attr("class");
+    if (classNames) {
+        classNames = $.trim(classNames).replace(/\s\s+/g, ' ').replace(/\s/gi, ".").split(".");
+    } else {
+        classNames = null;
+    }
+
+
+    var idx = null;
+    var similar = $(e).parent().children(nodeType);
+    for (var i = 0; i < similar.length; i++) {
+        if ($(similar[i]).is($(e))) {
+            idx = i;
+            break;
+        }
+    }
+
+    return new NodeAttributes(nodeType, classNames, id, idx);
+
+}
 
 jQuery.noConflict();
 $ = jQuery;
-$('div').click(function(e){
-	e.stopPropagation();
-  console.log(getSelectorPath($(e.target)));
+$('div').click(function(e) {
+    e.stopPropagation();
+    console.log(getSelectorPath($(e.target)));
 });
 
 
@@ -66,11 +124,10 @@ $('div').click(function(e){
 
 /*function highlight(e, n){
 	if($(e).prop('tagName')){
-    console.log(getFindSelector(e));
-    $(e).css('border', '2px solid blue');
-    setTimeout(function(){
-    	highlight($(e).parent().closest('div'), n + 1);}
-      , 1000);
+	console.log(getFindSelector(e));
+	$(e).css('border', '2px solid blue');
+	setTimeout(function(){
+		highlight($(e).parent().closest('div'), n + 1);}
+	  , 1000);
   }
 };*/
-
