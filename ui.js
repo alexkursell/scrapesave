555b444678df1ff80c7b115529480f4146a689a9
Alex Kursell <alexkursell@gmail.com>
2017-03-30 11:18:25 -0400

    Add files, get money.

--- ui.js
+++ ui.js
@@ -1,9 +1,27 @@
+$(document).ready(function(){
+		
+		$('#title').click(function(){
+			console.log("<tr>" + $('#titleinput').val() + "</tr>");
+			$('#titlelist').append("<tr><td>" + $('#titleinput').val() + "</td></tr>");
+			
+		});
+		
+		$("#titlelist").delegate('td','mouseover mouseleave', function(e) {
+			if (e.type == 'mouseover') {
+			  $(this).parent().addClass("highlight");
+			  $("colgroup").eq($(this).index()).addClass("highlight");
+			}
+			else {
+			  $(this).parent().removeClass("highlight");
+			  $("colgroup").eq($(this).index()).removeClass("highlight");
+			}
+		});
 
-$('#title').click(function(){
-	console.log("CLICKED");
-});
-
-random = 2;
-console.log("HI");
-
-
+		$("#urlbutton").click(function(){
+			console.log($("#urlbox").val());
+		});
+		
+		
+		
+		
+		});
